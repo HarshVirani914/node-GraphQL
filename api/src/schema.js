59d8febc1d2605e232c90c659de6bd91cb400b7d
harsh virani <harshvirani.91@gmail.com>
2022-11-14 16:05:43 +0530

    added-schemas

--- api/src/schema.js
+++ api/src/schema.js
@@ -1,11 +1,30 @@
-const { gql } = require('apollo-server')
+const { gql } = require("apollo-server");
 
 /**
  * Type Definitions for our Schema using the SDL.
  */
 const typeDefs = gql`
-  
+  type User {
+    id: ID!
+    username: String!
+  }
 
+  type Pet {
+    id: ID!
+    createdAt: String!
+    name: String!
+    type: String!
+  }
+
+  input NewPetInput {
+    name: String
+    type: String
+  }
+
+  type Query {
+    pets(input: NewPetInput): [Pet]!
+    pet(input: NewPetInput): Pet
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
