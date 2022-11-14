156f758735058601d40df34652dcd5008e008762
harsh virani <harshvirani.91@gmail.com>
2022-11-14 16:06:03 +0530

    added-resolvers

--- api/src/resolvers.js
+++ api/src/resolvers.js
@@ -5,19 +5,22 @@
 
 module.exports = {
   Query: {
-    
+    pets(_, { input }, context) {
+      return context.models.Pet.findMany(input);
+    },
+    pet(_, { input }, context) {
+      return context.models.Pet.findOne(input);
+    },
   },
-  Mutation: {
-    
-  },
-  Pet: {
-    img(pet) {
-      return pet.type === 'DOG'
-        ? 'https://placedog.net/300/300'
-        : 'http://placekitten.com/300/300'
-    }
-  },
-  User: {
-    
-  }
-}
+  // Mutation: {
+
+  // },
+  // Pet: {
+  //   img(pet) {
+  //     return pet.type === "DOG"
+  //       ? "https://placedog.net/300/300"
+  //       : "http://placekitten.com/300/300";
+  //   },
+  // },
+  // User: {},
+};
